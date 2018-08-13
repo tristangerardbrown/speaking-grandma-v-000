0d873e7a0d415488e3bc4a6a93ad1872d13da843
Tristan Brown <tgbrown@stanford.edu>
2018-08-13 21:49:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.downcase
-      return "HUH?! SPEAK UP, SONNY!."
-  elsif string == string.capitalize
-    return "HUH?! SPEAK UP, SONNY!."
   elsif string == string.upcase
       return "NO, NOT SINCE 1938!"
+    else 
+      return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
