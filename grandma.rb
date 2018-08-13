a4db242bd1e882119b5070992e0a9c4d31ccf8c1
Tristan Brown <tgbrown@stanford.edu>
2018-08-13 21:43:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,18 @@
 def speak_to_grandma(string)
   if string = string.downcase
-    return HUH?! SPEAK UP, SONNY!.
+    return "HUH?! SPEAK UP, SONNY!."
   elsif string = string.upcase
-    return NO, NOT SINCE 1938!
+    return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
-    return I LOVE YOU TOO PUMPKIN!
+    return "I LOVE YOU TOO PUMPKIN!"
   end 
-end 
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
